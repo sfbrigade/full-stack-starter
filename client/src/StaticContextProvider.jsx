f85d53bd7526faca29ccd36ad7bf6ea390f47e0a
Francis Li <francisli@users.noreply.github.com>
2025-10-06 12:50:58 -0700

    [Closes #4] Remove prop-types references, ran npm audit fix (#6)

--- client/src/StaticContextProvider.jsx
+++ client/src/StaticContextProvider.jsx
@@ -1,14 +1,7 @@
-import PropTypes from 'prop-types';
-
 import { staticContext } from './StaticContext';
 
 function StaticContextProvider ({ value, children }) {
   return <staticContext.Provider value={value}>{children}</staticContext.Provider>;
 }
-
-StaticContextProvider.propTypes = {
-  children: PropTypes.node,
-  value: PropTypes.object,
-};
 
 export default StaticContextProvider;
