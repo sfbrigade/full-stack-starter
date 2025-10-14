2ad610979997738f50ede2843e6cdefc9f0ea1d8
Francis Li <mail@francisli.com>
2025-10-14 15:36:55 -0700

    Refactor forgot password form

--- client/src/Passwords/ForgotPassword.jsx
+++ client/src/Passwords/ForgotPassword.jsx
@@ -1,25 +1,38 @@
-import { useState } from 'react';
 import { useNavigate, Link } from 'react-router';
-import { Box, Button, Container, Group, Stack, TextInput, Title } from '@mantine/core';
+import { Alert, Box, Button, Container, Group, Stack, TextInput, Title } from '@mantine/core';
+import { isEmail, useForm } from '@mantine/form';
+import { useMutation } from '@tanstack/react-query';
 import { Head } from '@unhead/react';
 
 import Api from '../Api';
+import ValidationError from '../ValidationError';
 
 function ForgotPassword () {
   const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const [showError, setShowError] = useState(false);
 
-  function onSubmit (event) {
-    event.preventDefault();
-    setShowError(false);
-    Api.passwords
-      .reset(email)
-      .then(() => {
-        navigate('/login', { state: { flash: 'Please check your email in a few minutes for a reset password link.' } });
-      })
-      .catch(() => setShowError(true));
-  }
+  const form = useForm({
+    initialValues: {
+      email: '',
+    },
+    validate: {
+      email: isEmail('Please enter a valid email address.'),
+    },
+  });
+
+  const onSubmitMutation = useMutation({
+    mutationFn: (values) => Api.passwords.reset(values.email),
+    onSuccess: () => navigate('/login', { state: { flash: 'Please check your email in a few minutes for a reset password link.' } }),
+    onError: (error) => {
+      if (error instanceof ValidationError) {
+        form.setErrors(error.data);
+      } else {
+        form.setErrors({
+          email: 'Email not found.',
+        });
+      }
+      window.scrollTo(0, 0);
+    },
+  });
 
   return (
     <>
@@ -28,17 +41,15 @@ function ForgotPassword () {
       </Head>
       <Container>
         <Title mb='md'>Forgot your password?</Title>
-        <form onSubmit={onSubmit}>
+        <form onSubmit={form.onSubmit(onSubmitMutation.mutateAsync)}>
           <Stack w={{ base: '100%', xs: 320 }}>
+            {form.errors.global && <Alert color='red'>{form.errors.global}</Alert>}
             <Box>Enter the email address you registered to receive a reset password link.</Box>
             <TextInput
+              {...form.getInputProps('email')}
+              key='email'
               label='Email'
               type='email'
-              id='email'
-              name='email'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              error={showError ? 'Email not found.' : ''}
             />
             <Group>
               <Button type='submit'>Submit</Button>
