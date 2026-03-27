f8a7d6b298d9bfa7231dcfc873eac966fb2149c1
Francis Li <mail@francisli.com>
2026-03-26 18:20:40 -0700

    Switch to new prisma-fixtures

--- server/prisma/client.js
+++ server/prisma/client.js
@@ -2,33 +2,36 @@ import { PrismaPg } from '@prisma/adapter-pg';
 
 import { Prisma, PrismaClient } from './generated/prisma/client.js';
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
+export { Prisma, PrismaPg, PrismaClient };
 
-export { Prisma };
-
-export const prisma = new PrismaClient({
-  adapter
-}).$extends({
-  name: 'paginate',
-  model: {
-    $allModels: {
-      async paginate ({ page, perPage, include, ...options }) {
-        const take = parseInt(perPage, 10);
-        const skip = (parseInt(page, 10) - 1) * take;
-        const context = Prisma.getExtensionContext(this);
-        const total = await context.count(options);
-        const records = await context.findMany({
-          ...options,
-          include,
-          skip,
-          take
-        });
-        return { records, total };
+export function createClient (connectionString) {
+  const adapter = new PrismaPg({
+    connectionString,
+  });
+  return new PrismaClient({
+    adapter
+  }).$extends({
+    name: 'paginate',
+    model: {
+      $allModels: {
+        async paginate ({ page, perPage, include, ...options }) {
+          const take = parseInt(perPage, 10);
+          const skip = (parseInt(page, 10) - 1) * take;
+          const context = Prisma.getExtensionContext(this);
+          const total = await context.count(options);
+          const records = await context.findMany({
+            ...options,
+            include,
+            skip,
+            take
+          });
+          return { records, total };
+        }
       }
     }
-  }
-});
+  });
+}
+
+export const prisma = createClient(process.env.DATABASE_URL);
 
 export default prisma;
